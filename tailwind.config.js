35bc63663ff59b8f923b571b1f76cae3f27c7c26
Bubbaz35 <110595971+Bubbaz35@users.noreply.github.com>
2025-03-23 17:06:38 +0000

    CSS Updates

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,88 @@
+const defaultTheme = require("tailwindcss/defaultTheme");
+/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/**/*.{html,js}', // Adjust this path as needed depending on your project
+    "./index.html", 
+    "./src/**/*.{js,ts,jsx,tsx}",
   ],
+  darkMode: "class", // Enables dark mode using the "class" strategy
   theme: {
-    extend: {},
+    fontFamily: {
+      outfit: ["Outfit", "sans-serif"], // Custom font
+    },
+    screens: {
+      "2xsm": "375px", // Custom small screen size
+      xsm: "425px",    // Custom screen size
+      "3xl": "2000px", // Custom large screen size
+      ...defaultTheme.screens, // Include default screen sizes
+    },
+    extend: {
+      fontSize: {
+        "title-2xl": ["72px", "90px"],
+        "title-xl": ["60px", "72px"],
+        "title-lg": ["48px", "60px"],
+        "title-md": ["36px", "44px"],
+        "title-sm": ["30px", "38px"],
+        "theme-xl": ["20px", "30px"],
+        "theme-sm": ["14px", "20px"],
+        "theme-xs": ["12px", "18px"],
+      },
+      colors: {
+        current: "currentColor",
+        transparent: "transparent",
+        white: "#FFFFFF",
+        black: "#101828",
+        brand: {
+          25: "#F2F7FF",
+          50: "#ECF3FF",
+          100: "#DDE9FF",
+          200: "#C2D6FF",
+          300: "#9CB9FF",
+          400: "#7592FF",
+          500: "#465FFF",
+          600: "#3641F5",
+          700: "#2A31D8",
+          800: "#252DAE",
+          900: "#262E89",
+          950: "#161950",
+        },
+        gray: {
+          dark: "#1A2231",
+          25: "#FCFCFD",
+          50: "#F9FAFB",
+          100: "#F2F4F7",
+          200: "#E4E7EC",
+          300: "#D0D5DD",
+          400: "#98A2B3",
+          500: "#667085",
+          600: "#475467",
+          700: "#344054",
+          800: "#1D2939",
+          900: "#101828",
+          950: "#0C111D",
+        },
+        blue: {
+          light: "#0BA5EC",
+        },
+      },
+      boxShadow: {
+        "theme-md":
+          "0px 4px 8px -2px rgba(16, 24, 40, 0.10), 0px 2px 4px -2px rgba(16, 24, 40, 0.06)",
+        "theme-lg":
+          "0px 12px 16px -4px rgba(16, 24, 40, 0.08), 0px 4px 6px -2px rgba(16, 24, 40, 0.03)",
+      },
+      spacing: {
+        4.5: "1.125rem",
+        5.5: "1.375rem",
+        6.5: "1.625rem",
+        7.5: "1.875rem",
+        8.5: "2.125rem",
+        9.5: "2.375rem",
+        10.5: "2.625rem",
+        11.5: "2.875rem",
+        12.5: "3.125rem",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms"), require("autoprefixer")],
 };
